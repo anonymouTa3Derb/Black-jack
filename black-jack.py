b93c7571c64c373a142d8ddd33ea6bfc897e2723
[mehdi] <[essakim2000@gmail.com]>
2025-04-11 16:08:39 +0200

    update black jack with count learing

--- black-jack.py
+++ black-jack.py
@@ -1,353 +1,342 @@
-#!/Users/Utsav/downloads/udemy python
-
-# For using the same code in either Python 2 or 3
-# from __future__ import print_function
-
-""" milestone_project_1.py: BlackJack Game """
-
-__author__ = "Utsav Shah"
-__copyright__ = "Copyright 2017, The Austin Side Project"
-__credits__ = ["Hustle"]
-__license__ = "UTS"
-__version__ = "1.1.0"
-__maintainer__ = "Utsav Shah"
-__email__ = "utsavshah507@gmail.com"
-__status__ = "Productive"
-
-## Milestone Project 2
-
-# Importing libraries -- used for shuffling cards
 import random
-
-# Boolean type to know whether play is in hand
-playing = False
-
-# Amount for buy-in
-chip_pool = 100
-# raw_input('Enter the amount for buy-in: ')
-print 'Your buy-in amount is: ',chip_pool
-
-bet = 1
-
-restart_phrase = "Press d to deal the cards again, or press q to quit."
-
-# Hearts, Diamonds, Clubs, Spades
-suits = ('H','D','S','C')
-
-# Possible Card Ranks
-ranking = ('A','2','3','4','5','6','7','8','9','10','J','Q','K')
-
-# Point Val Dict (Dual existence of Ace is defined later)
-card_val = {'A':1, '2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, '10':10, 'J':10, 'Q':10, 'K':10}
-
-# Creating Card Class
-
-class Card:
-
-	def __init__(self, suit, rank):
-		self.suit = suit
-		self.rank = rank
-
-	def __str__(self):
-		return self.suit + self.rank
-
-	def grab_suit(self):
-		return self.suit
-
-	def grab_rank(rank):
-		return self.rank
-
-	def draw(self):
-		print (self.suit + self.rank)
-
-# Creating Hand Class
-# Gives dual existence to Ace
-
-class Hand:
-
-	def __init__(self):
-		self.cards = []
-		self.value = 0
-
-		# Aces can be 1 0r 11 as defined below
-		self.ace = False
-
-	def __str__(self):
-		'''Return a string of current hand composition'''
-		hand_comp = ""
-
-		# List Comprehension
-		for card in self.cards:
-			card_name = card.__str__()
-			hand_comp += " " + card_name
-
-		return 'The hand has %s' %hand_comp
-
-	def card_add(self,card):
-		'''Add another card to the hand'''
-		self.cards.append(card)
-
-		# Checking for Aces
-		if card.rank == 'A':
-			self.ace = True
-		self.value += card_val[card.rank]
-
-	def calc_val(self):
-		'''Calculating value of hand, making aces = 1 if they don't bust the hand'''
-
-		if (self.ace == True and self.value < 12):
-			return self.value + 10
-		else:
-			return self.value
-
-	def draw(self, hidden):
-		if hidden == True and playing == True:
-			# Don't show first hidden card
-			starting_card = 1
-		else:
-			starting_card = 0
-		for x in range(starting_card, len(self.cards)):
-			self.cards[x].draw()
-
-# Creating Class Deck
-
-class Deck:
-
-	def __init__(self):
-		'''Creating a deck in order'''
-		self.deck = []
-		for suit in suits:
-			for rank in ranking:
-				self.deck.append(Card(suit,rank))
-
-	def shuffle(self):
-		'''Shuffles the deck, using python's built-in random library'''
-		random.shuffle(self.deck)
-
-	def deal(self):
-		'''Grabbing the first item in the deck'''
-		single_card = self.deck.pop()
-		return single_card
-
-	def __str__(self):
-		deck_comp = " "
-		for card in self.cards:
-			deck_comp += " " + deck_comp.__str__()
-
-		return "The deck has " + deck_comp
-
-# End of Classes
-
-# First Bet
-
-def make_bet():
-	'''Ask the player for the bet amount and '''
-
-	global bet
-	bet = 0
-
-	print 'What amount of chips would you like to bet? (Please enter whole integer) '
-
-	# While loop to keep asking for the bet
-	while bet == 0:
-		# Using bet_comp as a checker
-		bet_comp = raw_input()
-		bet_comp = int(bet_comp)
-
-		# Check to make sure the bet is within the remaining amount of chips left
-		if bet_comp >= 1 and bet_comp <= chip_pool:
-			bet = bet_comp
-		else:
-			print "Invalid bet, you only have " + str(chip_pool) + " remaining"
-
-def deal_cards():
-	'''This function deals out cards and sets up round'''
-
-	# Set up all global variables
-	global result, playing, deck, player_hand, dealer_hand, chip_pool, bet
-
-	# Creating a deck
-	deck = Deck()
-
-	# Shuffle it
-	deck.shuffle()
-
-	# Set up the bet
-	make_bet()
-
-	# Set up both player and dealer hands
-	player_hand = Hand()
-	dealer_hand = Hand()
-
-	# Deal out initial cards
-	player_hand.card_add(deck.deal())
-	player_hand.card_add(deck.deal())
-
-	result = "Hit or Stand? Press h for hit or s for stand: "
-
-	if playing == True:
-		print 'Fold, Sorry'
-		chip_pool -= bet
-
-	# Set up to know currently playing hand
-	playing = True
-	game_step()
-
-
-# Hit Function
-
-def hit():
-	'''Implementing the hit button'''
-
-	global playing, chip_pool, deck, player_hand, dealer_hand, result, bet
-
-	# If hand is in play add card
-	if playing:
-		if player_hand.calc_val() <= 21:
-			player_hand.card_add(deck.deal())
-
-		print "Player hand is %s" %player_hand
-
-		if player_hand.calc_val() > 21:
-			result = 'Busted!' + restart_phrase
-
-			chip_pool -= bet
-			playing = False
-
-	else:
-		result = "Sorry, can't hit"	+ restart_phrase
-
-	game_step()
-
-# Stand Function
-
-def stand():
-	global playing, chip_pool, deck, player_hand, dealer_hand, result, bet
-
-	'''This function plays the dealers hand, since stand was chosen'''
-
-	if playing == False:
-		if player_hand.calc_val() > 0:
-			result = "Sorry, you can't stand!"
-
-	# Going through all other possible options
-	else:
-
-		# Sfot 17 Rule
-		while dealer_hand.calc_val() < 17:
-			dealer_hand.card_add(deck.deal())
-
-		# Dealer Busts
-		if dealer_hand.calc_val() > 21:
-			result = 'Dealer busts! You win! ' + restart_phrase
-
-			chip_pool += bet
-			playing = False
-
-		# Player has better hand than dealer
-		elif dealer_hand.calc_val() < player_hand.calc_val():
-			result = 'You beat the dealer, you win! ' + restart_phrase
-			chip_pool += bet
-			playing = False
-
-		# Push
-		elif dealer_hand.calc_val == player_hand.calc_val():
-			result = 'Tied up, push!' + restart_phrase
-			playing = False
-
-		# Dealer beats player
-		else:
-			result = 'Dealer Wins! ' + restart_phrase
-			chip_pool -= bet
-			playing = False
-
-	game_step()
-
-# Function to print results and ask user for next step
-
-def game_step():
-	'''Function to print game step/status on output'''
-
-	# Display Player Hand
-	print ""
-	print ('Player Hand is: '),player_hand.draw(hidden = False)
-
-	print ''
-	print 'Player hand total is: ' +str(player_hand.calc_val())
-
-	# Display Dealer Hand
-	print ''
-	print('Dealer Hand is: '), dealer_hand.draw(hidden = True)
-
-	# If game round is over
-	if playing == False:
-		print " --- for a total of " + str(dealer_hand.calc_val())
-		print "Chip Total: " +str(chip_pool)
-
-	# Otherwise, don't know the second card yet
-	else:
-		print " with another card hidden upside down"
-
-	# Print result of hit or stand
-	print ''
-	print result
-
-	player_input()
-
-
-# Function to exit the game
-
-def game_exit():
-	print 'Thanks for playing!'
-	exit()
-
-# Function to read user input
-
-def player_input():
-	'''Read user input, lower case it jsuts to be safe'''
-
-	plin = raw_input().lower()
-
-	if plin == 'h':
-		hit()
-	elif plin == 's':
-		stand()
-	elif plin == 'd':
-		deal_cards()
-	elif plin == 'q':
-		game_exit()
-	else:
-		print "Invalid Input. Enter h, s, d, or q: "
-		player_input()
-
-# Intro to game
+import sys
+
+# =========================================
+# Variables globales
+# =========================================
+
+# Indique si la partie est en cours (True) ou terminée (False)
+en_jeu = False
+
+# Montant initial de jetons (buy-in)
+solde_jetons = 100
+print("Votre montant de départ est :", solde_jetons)
+
+# Mise par défaut
+mise = 1
+
+# Texte de relance
+message_relance = "Appuyez sur (d) pour redistribuer ou (q) pour quitter."
+
+# Enseignes : Cœurs (C), Carreaux (D), Piques (P), Trèfles (T)
+enseigne_possibles = ('C', 'D', 'P', 'T')
+
+# Valeurs possibles des cartes
+valeurs_possibles = ('A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K')
+
+# Dictionnaire des valeurs en points (l'As sera géré plus finement dans la classe Main)
+dico_valeurs = {
+    'A': 1,
+    '2': 2,
+    '3': 3,
+    '4': 4,
+    '5': 5,
+    '6': 6,
+    '7': 7,
+    '8': 8,
+    '9': 9,
+    '10': 10,
+    'J': 10,
+    'Q': 10,
+    'K': 10
+}
+
+# =========================================
+# Comptage de cartes (Hi-Lo)
+# =========================================
+running_count = 0  # Compteur de cartes global
+
+def compter_carte(valeur_carte):
+    """
+    Met à jour le compteur (running_count) selon le système Hi-Lo.
+    Valeur_carte est la valeur type 'A', '2', ..., '10', 'J', 'Q', 'K'.
+    """
+    global running_count
+
+    if valeur_carte in ('2', '3', '4', '5', '6'):
+        running_count += 1
+    elif valeur_carte in ('10', 'J', 'Q', 'K', 'A'):
+        running_count -= 1
+    # Cartes 7, 8, 9 ne changent pas le count (0)
+
+# Fonction pour donner un conseil (très simplifié) en fonction du count
+def conseil_count():
+    """
+    Retourne un conseil basé sur la valeur du running_count.
+    Ceci est très basique, juste pour l’exemple.
+    """
+    global running_count
+    if running_count > 2:
+        return f"Le compte est positif (+{running_count}) : Le jeu est en votre faveur, vous pouvez miser plus !"
+    elif running_count < -2:
+        return f"Le compte est négatif ({running_count}) : Attention, le croupier est avantagé. Baissez la mise ou soyez prudent."
+    else:
+        return f"Le compte est proche de l’équilibre ({running_count}). Jouez normalement."
+
+# =========================================
+# Classe Carte
+# =========================================
+
+class Carte:
+    def __init__(self, enseigne, valeur):
+        self.enseigne = enseigne  # 'C', 'D', 'P', 'T'
+        self.valeur = valeur      # 'A', '2', '3', ..., 'J', 'Q', 'K'
+
+    def __str__(self):
+        return f"{self.enseigne}{self.valeur}"
+
+    def afficher_carte(self):
+        print(str(self))
+
+# =========================================
+# Classe Main (Hand)
+# =========================================
+
+class Main:
+    """
+    Représente la main d'un joueur ou du croupier.
+    L’As peut valoir 1 ou 11 (géré dans la méthode calculer_valeur).
+    """
+
+    def __init__(self):
+        self.cartes = []
+        self.total = 0
+        self.possede_as = False
+
+    def __str__(self):
+        composition = " ".join(str(carte) for carte in self.cartes)
+        return f"La main contient : {composition}"
+
+    def ajouter_carte(self, carte):
+        """
+        Ajoute une carte à la main et met à jour la valeur ET
+        met à jour le compteur Hi-Lo.
+        """
+        self.cartes.append(carte)
+        # Mise à jour de la valeur
+        if carte.valeur == 'A':
+            self.possede_as = True
+        self.total += dico_valeurs[carte.valeur]
+        # Mise à jour du count
+        compter_carte(carte.valeur)
+
+    def calculer_valeur(self):
+        """
+        Calcule la valeur de la main en considérant l’As comme 1 ou 11.
+        """
+        if self.possede_as and self.total <= 11:
+            return self.total + 10
+        else:
+            return self.total
+
+    def afficher_main(self, cacher_premiere=False):
+        if cacher_premiere and en_jeu:
+            print("Carte cachée")
+            for carte in self.cartes[1:]:
+                carte.afficher_carte()
+        else:
+            for carte in self.cartes:
+                carte.afficher_carte()
+
+# =========================================
+# Classe Paquet (Deck)
+# =========================================
+
+class Paquet:
+    def __init__(self):
+        self.paquet = []
+        for enseigne in enseigne_possibles:
+            for valeur in valeurs_possibles:
+                self.paquet.append(Carte(enseigne, valeur))
+
+    def melanger(self):
+        random.shuffle(self.paquet)
+
+    def distribuer(self):
+        return self.paquet.pop()
+
+    def __str__(self):
+        return " ".join(str(carte) for carte in self.paquet)
+
+# =========================================
+# Fonctions du jeu
+# =========================================
+
+def demander_mise():
+    global mise, solde_jetons
+
+    mise = 0
+    print("\nQuel montant souhaitez-vous miser ? (Entrez un entier) :")
+
+    while True:
+        try:
+            saisie = input(">> ")
+            montant = int(saisie)
+            if 1 <= montant <= solde_jetons:
+                mise = montant
+                break
+            else:
+                print(f"Mise invalide. Vous avez {solde_jetons} jetons disponibles.")
+        except ValueError:
+            print("Veuillez entrer un nombre entier.")
+
+def distribuer_cartes():
+    """
+    Distribue les cartes pour commencer un nouveau tour.
+    """
+    global en_jeu, paquet, main_joueur, main_croupier
+    global solde_jetons, mise, resultat
+    global running_count  # On veut afficher l'état du compteur
+
+    # Avant de créer un nouveau paquet, on peut décider
+    # si l’on veut remettre le count à 0 quand on recharge un deck :
+    # Pour simplifier, on va considérer qu’on reprend un deck neuf à chaque manche,
+    # donc on réinitialise le running_count. À vous d’adapter selon vos règles.
+    running_count = 0
+
+    paquet = Paquet()
+    paquet.melanger()
+
+    demander_mise()
+
+    main_joueur = Main()
+    main_croupier = Main()
+
+    # Distribue deux cartes au joueur
+    main_joueur.ajouter_carte(paquet.distribuer())
+    main_joueur.ajouter_carte(paquet.distribuer())
+
+    # Distribue deux cartes au croupier
+    main_croupier.ajouter_carte(paquet.distribuer())
+    main_croupier.ajouter_carte(paquet.distribuer())
+
+    resultat = "Voulez-vous tirer (h) ou rester (s) ?"
+    en_jeu = True
+
+    etat_partie()
+
+def tirer_carte():
+    global en_jeu, solde_jetons, paquet, main_joueur, resultat, mise
+
+    if en_jeu:
+        if main_joueur.calculer_valeur() <= 21:
+            main_joueur.ajouter_carte(paquet.distribuer())
+
+        if main_joueur.calculer_valeur() > 21:
+            print(f"Votre main est de {main_joueur.calculer_valeur()}. Vous brûlez !")
+            solde_jetons -= mise
+            resultat = f"Vous avez perdu cette manche. {message_relance}"
+            en_jeu = False
+        else:
+            resultat = "Voulez-vous tirer (h) ou rester (s) ?"
+    else:
+        resultat = f"Désolé, vous ne pouvez plus tirer de carte. {message_relance}"
+
+    etat_partie()
+
+def rester():
+    global en_jeu, solde_jetons, paquet, main_croupier, main_joueur, resultat, mise
+
+    if not en_jeu:
+        resultat = "La manche est déjà terminée."
+        etat_partie()
+        return
+
+    # Le croupier tire tant qu'il a moins de 17
+    while main_croupier.calculer_valeur() < 17:
+        main_croupier.ajouter_carte(paquet.distribuer())
+
+    total_croupier = main_croupier.calculer_valeur()
+    total_joueur = main_joueur.calculer_valeur()
+
+    if total_croupier > 21:
+        resultat = f"Le croupier dépasse 21 ({total_croupier}). Vous gagnez ! {message_relance}"
+        solde_jetons += mise
+    elif total_croupier < total_joueur:
+        resultat = f"Vous avez {total_joueur} contre {total_croupier} pour le croupier. Vous gagnez ! {message_relance}"
+        solde_jetons += mise
+    elif total_croupier == total_joueur:
+        resultat = f"Égalité ! (push) {message_relance}"
+    else:
+        resultat = f"Le croupier a {total_croupier} et vous {total_joueur}. Le croupier gagne. {message_relance}"
+        solde_jetons -= mise
+
+    en_jeu = False
+    etat_partie()
+
+def quitter_jeu():
+    print("Merci d'avoir joué !")
+    sys.exit(0)
+
+def etat_partie():
+    """
+    Affiche l'état actuel de la partie (mains, compte) et
+    donne un conseil basé sur le compte.
+    """
+    print("\n==== État de la partie ====")
+
+    # Afficher la main du joueur
+    print("Main du joueur :")
+    main_joueur.afficher_main(cacher_premiere=False)
+    print(f"Total joueur = {main_joueur.calculer_valeur()}")
+
+    # Afficher la main du croupier
+    print("\nMain du croupier :")
+    main_croupier.afficher_main(cacher_premiere=en_jeu)
+    if not en_jeu:
+        # Partie terminée, on affiche le total croupier
+        print(f"Total croupier = {main_croupier.calculer_valeur()}")
+
+    # Afficher le solde
+    print(f"\nVous avez maintenant {solde_jetons} jetons.")
+
+    # Afficher le running_count et un conseil
+    print(f"Running count (Hi-Lo) = {running_count}")
+    print("Conseil basé sur le compte :", conseil_count())
+
+    # Afficher le résultat ou la question en cours
+    print("\n" + str(resultat))
+
+    # Lire la commande
+    commande_joueur()
+
+def commande_joueur():
+    """
+    Lit la commande du joueur et exécute l’action correspondante.
+    """
+    choix = input(">> ").lower()
+
+    if choix == 'h':
+        tirer_carte()
+    elif choix == 's':
+        rester()
+    elif choix == 'd':
+        distribuer_cartes()
+    elif choix == 'q':
+        quitter_jeu()
+    else:
+        print("Choix invalide. Veuillez entrer : (h) tirer, (s) rester, (d) distribuer, (q) quitter.")
+        commande_joueur()
 
 def intro():
-	statement = '''Welcome to BlackJack! Get as close to 21 as you can without getting over! 
-Dealer hits until she reaches 17. Aces count as 1 or 11. Card output goes a letter followed by a number of face notation. '''
+    regles = """
+Bienvenue au BlackJack (avec comptage de cartes Hi-Lo) !
+Le but est de vous approcher le plus possible de 21 sans dépasser.
+Le croupier tire jusqu’à avoir au moins 17.
+Les As valent 1 ou 11, selon ce qui vous avantage.
+Comptage Hi-Lo :
+  - 2 à 6 = +1
+  - 7 à 9 =  0
+  - 10, J, Q, K, A = -1
+Plus le compte est élevé, plus le deck vous avantage.
+Appuyez sur (h) pour tirer, (s) pour rester, (d) pour distribuer, ou (q) pour quitter.
+"""
+    print(regles)
 
-	print statement
-	print ''
+# =========================================
+# Lancement du jeu
+# =========================================
 
-# Playing the Game
-
-'''The following code will initiate the game! 
-(Note: Need to Run a 11 Cells)'''
-
-# Create a Deck
-deck = Deck()
-
-# Shuffle it
-deck.shuffle()
-
-# Create player and dealer hands
-print ''
-player_hand = Hand()
-print ''
-deal_hand = Hand()
-
-# Print the intro
-intro()
-
-# Deal out the cards and start the game!
-deal_cards()
+if __name__ == "__main__":
+    intro()
+    distribuer_cartes()
